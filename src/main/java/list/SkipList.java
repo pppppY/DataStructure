a949561be70b732a23dc43ee2ba8ed45ea875370
yangpeng <pengyang813@foxmail.com>
2016-10-25 19:47:03 +0800

    SkipList-todo

--- src/main/java/list/SkipList.java
+++ src/main/java/list/SkipList.java
@@ -4,5 +4,17 @@ package list;
  * Created by py on 16-9-22.
  */
 public class SkipList {
+//    
+}
+class ListNode{
+    public static ListNode NIL = new ListNode();
+    public ListNode []forward;
+    public int key;
 
+    public ListNode() {
+    }
+
+    public ListNode(int maxLevel, int key) {
+
+    }
 }
